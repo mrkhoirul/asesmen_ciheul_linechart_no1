c627d990271171671accdfcda916ca83e7293fb7
Khoirul Azkiya <khoirul.azkya@gmail.com>
2025-08-11 13:26:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bokeh.plotting import figure, show
 from bokeh.models import DatetimeTickFormatter
 import re
 from datetime import datetime
+from collections import defaultdict
 
 # Reading File
 with open('data.txt', 'r') as file:
@@ -11,19 +12,26 @@ with open('data.txt', 'r') as file:
 reg = r"Timestamp:\s*([0-9\-]+\s+[0-9:]+).*?\[\s\s\d\]\s+[0-9]*\.[0-9]+-[0-9]*\.[0-9]+\s+sec\s+[\d\.]+\s+\w+Bytes\s+([\d\.]+)\s+(Mbits|Kbits)\/sec.*?sender"
 matches = re.findall(reg, data, re.S)
 
-timestamps = []
-bitrates = []
-
+# Parsing the matches to extract timestamps and bitrates & convert to Mbits
+data_in_hour = defaultdict(list)
 for timestamp, value, unit in matches:
     # Convert timestamp to datetime object
     dt = datetime.strptime(timestamp.strip(), '%Y-%m-%d %H:%M:%S')
-    timestamps.append(dt)
+    hour = dt.replace(minute=0, second=0, microsecond=0)
     
     # Convert bitrate to float and adjust based on unit
+    val = float(value)
     if unit == 'Kbits':
-        bitrates.append(float(value) / 1000.0)  # Convert Kbits to Mbits
-    else:
-        bitrates.append(float(value))
+        val /= 1000.0  # Convert Kbits to Mbits
+    data_in_hour[hour].append(val)
+
+# Take the data for each hour
+timestamps = []
+bitrates = []
+for hour in sorted(data_in_hour.keys()):
+    average = sum(data_in_hour[hour]) / len(data_in_hour[hour])
+    timestamps.append(hour)
+    bitrates.append(average)
 
 # Creating the Bokeh Figure
 p = figure(
